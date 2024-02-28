b1a62acf8e0cb71a13746348381353c8215a118a
the-nayshe-5 <135449013+the-nayshe-5@users.noreply.github.com>
2024-02-29 05:16:47 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
 
 const Stack = createNativeStackNavigator();
 
