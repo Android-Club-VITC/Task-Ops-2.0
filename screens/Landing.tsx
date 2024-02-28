34e586529258a30920a7d74c5864a204e8491886
Stephen Stolk <shikharburman2@gmail.com>
2024-02-28 23:59:24 +0530

    Updated Landing Page

--- screens/Landing.tsx
+++ screens/Landing.tsx
@@ -1,24 +1,22 @@
-import { View, Text, SafeAreaView, TouchableOpacity, TouchableHighlight } from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import { Image } from "expo-image";
 import * as React from "react";
 const LandingPage = () => {
   return (
     <>
       <SafeAreaView className="bg-black flex-1 w-full">
-        <View className="flex w-[full] mx-auto top-[103px] right-[8%]">
-        <Image
-          className="w-[375px] h-[196] mx-auto"
-          source={require("../assets/landingtop.jpg")
-          }
-          contentFit="contain"
-        />
+        <View className="flex justify-center items-center w-[100%] mx-auto top-[15%]">
+          <Image
+            className="w-[375px] h-[196px]"
+            source={require("../assets/landingtop.jpg")}
+            contentFit="contain"
+          />
+          <Image
+            className="w-[240px] h-[180px] mx-auto absolute top-[15%]"
+            source={require("../assets/Landing-text.svg")}
+            contentFit="contain"
+          />
         </View>
-        <Text className="flex top-[22%] mx-auto w-full absolute text-[45px] text-center text-[#3ACCCC] font-normal shadow-2xl shadow-black">
-        TASK OPS
-        </Text>
-        <Text className="top-[30%] right-[20%] mx-auto w-full absolute text-[45px] text-center text-[#3ACCCC] font-normal shadow-2xl shadow-black">
-          2.0
-        </Text>
 
         <View className="w-full flex h-56 flex-row top-[20%]">
           <View className="w-1/2 h-full mx-auto mt-12 border-r-2 border-r-white">
@@ -44,31 +42,32 @@ const LandingPage = () => {
           </View>
         </View>
 
-        <View className="z-20 rotate-90 h-36 w-16 bg-white rounded-[15px] shadow-lg mt-[40%] mx-auto">
-          <TouchableOpacity className="h-20 rounded-t-md mx-auto rotate-90">
-            <Image className="h-16 w-16 bg-white z-[50] mx-auto my-auto rounded-md"
-              source={require("../assets/left.svg")}
-              contentFit="contain"
-            />
-            </TouchableOpacity>
-
-            <Image
-          className="rotate-90 w-8 h-12 bg-white my-auto mx-auto"
-          source={require("../assets/bars.svg")}
-          contentFit="contain"
-        />
-
-             <TouchableOpacity className="-rotate-90 h-20 rounded-t-md mx-auto">
-            <Image className="h-16 w-16 bg-white z-[50] mx-auto my-auto rounded-md"
-              source={require("../assets/left.svg")}
-              contentFit="contain"
-            />
+        <View className="w-[40%] h-[10%] bg-white -bottom-[55%] flex-row items-center mx-auto justify-center rounded-xl z-[10]">
+          <TouchableOpacity className="mx-auto h-16 w-[50%]">
+          <Image
+            className="h-16 w-[100%]"
+            source={require("../assets/left.svg")}
+            contentFit="contain"
+          />
           </TouchableOpacity>
-         
+
+          <Image
+            className="h-16 w-4"
+            source={require("../assets/bars.svg")}
+            contentFit="contain"
+          />
+
+         <TouchableOpacity className="mx-auto h-16 w-[50%]">
+         <Image
+            className="h-16 w-[100%]"
+            source={require("../assets/right.svg")}
+            contentFit="contain"
+          />
+         </TouchableOpacity>
         </View>
-        
+
         <Image
-          className="absolute  -bottom-[15%] w-screen h-72 max-h-screen right-[7%]"
+          className="absolute -bottom-[15%] w-screen h-72 max-h-screen right-[7%]"
           source={require("../assets/landingcircle.png")}
           contentFit="contain"
         />
