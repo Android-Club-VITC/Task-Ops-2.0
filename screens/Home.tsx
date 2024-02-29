500eeb76a15db3df7393bf0ccbd9b2666252e924
Pranshu Aggarwal <70687348+Pranshu1902@users.noreply.github.com>
2024-02-29 20:04:05 +0530

    remove task api methods

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -2,7 +2,7 @@ import { View, Text, ScrollView } from "react-native";
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Task } from "../api/models";
-import { getAllTasks } from "../api/tasks";
+// import { getAllTasks } from "../api/tasks";
 import LandingPage from "./Landing";
 import LeaderBoard from "./LeaderBoard";
 import Progress from "./Progress";
@@ -10,12 +10,12 @@ import Progress from "./Progress";
 export default function Home() {
   const [tasks, setTasks] = useState<Task[]>([]); // Make sure to create types globally instead of using any
 
-  useEffect(() => {
-    (async () => {
-      const tasks = await getAllTasks();
-      setTasks(tasks);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const tasks = await getAllTasks();
+  //     setTasks(tasks);
+  //   })();
+  // }, []);
 
   const navigation = useNavigation();
 
