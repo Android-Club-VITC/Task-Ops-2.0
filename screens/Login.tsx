0aeacd86a55dc9655e808ab8b9fe5ca435188f01
Pranshu1902 <pranshu1902@gmail.com>
2024-02-29 23:04:08 +0530

    refine UI

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,70 +1,95 @@
-import React, { useState, useCallback } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import Background from '../components/Background';
-import Input from '../components/Input';
-import Button from '../components/Button';
-
+import React, { useState, useCallback, useContext } from "react";
+import { StyleSheet, Text, View, TextInput, ToastAndroid } from "react-native";
+// import { useFonts } from 'expo-font';
+// import * as SplashScreen from 'expo-splash-screen';
+import Background from "../components/Background";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { loginTeam } from "../api/teams";
+import { UserContext } from "../context/UserContext";
+import { delay } from "../utils/helpers";
 //SplashScreen.preventAutoHideAsync();
 
 const Login = () => {
-  // const [isLoaded] = useFonts({
-  //   Aldrich: require("../assets/fonts/Aldrich/Aldrich-Regular.ttf"),
-  //   Raleway: require("../assets/fonts/Raleway/Raleway-VariableFont_wght.ttf"),
-  // });
+  const [teamName, setTeamName] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+  const { setUserId } = useContext(UserContext);
 
-  // const handleOnLayout = useCallback(async () => {
-  //   if (isLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [isLoaded]);
+  const submit = async () => {
+    if (!teamName || !password) {
+      ToastAndroid.show("Please fill all the fields", ToastAndroid.SHORT);
+      return;
+    }
 
-  // if (!isLoaded) {
-  //   return null;
-  // }
-  var [isPress, setIsPress] = useState(false);
+    try {
+      const res = await loginTeam(teamName, password);
+
+      ToastAndroid.show("Team logged in successfully!", ToastAndroid.SHORT);
+
+      delay(500);
+
+      await setUserId(res.id);
+    } catch (e) {
+      ToastAndroid.show((e as any).message, ToastAndroid.SHORT);
+    }
+  };
 
   return (
     <Background>
       <View style={styles.container}>
-        <Text style={styles.heading}>
-          LOGIN
-        </Text>
-        <View style={styles.input}>
-          <Input keyboardType='number-pad' placeholder='Team ID'></Input>
+        <Text style={styles.heading}>LOGIN</Text>
+        <View className="relative w-full top-5 inset-0 mx-auto my-auto space-y-10">
+          <TextInput
+            className="w-[70%] mx-auto h-12 bg-white rounded-xl text-2xl p-2"
+            placeholder="Team Name"
+            onChange={(e) => {
+              setTeamName(e.nativeEvent.text);
+            }}
+          />
+          <TextInput
+            textContentType="password"
+            secureTextEntry={true}
+            className="w-[70%] mx-auto h-12 bg-white rounded-xl text-2xl p-2"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.nativeEvent.text);
+            }}
+          />
         </View>
-        <Button label={"SUBMIT"} TextColor={ isPress ? "#43FFFF" : "black" }  Margin={100} Press={()=>{}} />
+        <Button
+          label={"SUBMIT"}
+          TextColor={"black"}
+          Margin={100}
+          Press={() => {}}
+        />
       </View>
-      
     </Background>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    width:400,
-    marginVertical:100,
-    display:"flex",
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center",
+    width: 400,
+    marginVertical: 100,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   heading: {
-    color:"#43FFFF",
-    fontSize:64,
+    color: "#43FFFF",
+    fontSize: 64,
     // top:108,
     // left:60,
-    textAlign:"center",
-    marginBottom:150,
+    textAlign: "center",
+    marginBottom: 150,
     //fontFace:"Aldrich",
   },
-  
-  input: {
-    marginTop: 120
-  }
 
+  input: {
+    marginTop: 120,
+  },
 });
 
 export default Login;
--- screens/Register.tsx
+++ screens/Register.tsx
@@ -1,68 +1,97 @@
-import React, { useState, useCallback } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import Background from '../components/Background';
-import Input from '../components/Input';
-import Button from '../components/Button';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, ToastAndroid } from "react-native";
+// import { useFonts } from "expo-font";
+// import * as SplashScreen from 'expo-splash-screen';
+import Background from "../components/Background";
+// import Input from "../components/Input";
+import Button from "../components/Button";
+import { registerTeam } from "../api/teams";
 
 //SplashScreen.preventAutoHideAsync();
 
 const Register = () => {
-  // const [isLoaded] = useFonts({
-  //   Aldrich: require("../assets/fonts/Aldrich/Aldrich-Regular.ttf"),
-  //   Raleway: require("../assets/fonts/Raleway/Raleway-VariableFont_wght.ttf"),
-  // });
+  const [teamName, setTeamName] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
+  const [roundNo, setRoundNo] = useState<string | null>(null);
 
-  // const handleOnLayout = useCallback(async () => {
-  //   if (isLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [isLoaded]);
+  const submit = async () => {
+    if (!teamName || !password || !roundNo) {
+      ToastAndroid.show("Please fill all the fields", ToastAndroid.SHORT);
+      return;
+    }
 
-  // if (!isLoaded) {
-  //   return null;
-  // }
-  var [isPress, setIsPress] = useState(false);
+    try {
+      const res = await registerTeam(teamName, password, parseInt(roundNo));
+
+      ToastAndroid.show(
+        "Team registered successfully, Please Login Now!",
+        ToastAndroid.SHORT
+      );
+    } catch (e) {
+      ToastAndroid.show((e as any).message, ToastAndroid.SHORT);
+    }
+  };
 
   return (
     <Background>
-      //onLayout={handleOnLayout}
+      {/* //onLayout={handleOnLayout} */}
       <View style={styles.container}>
-        <Text style={styles.heading}>
-          REGISTER
-        </Text>
-        <View>
-          <Input placeholder='Team Name'></Input>
-          <Input keyboardType='number-pad' placeholder='Team ID'></Input>
-          <Input keyboardType='number-pad' placeholder='Team Slot'></Input>
+        <Text style={styles.heading}>REGISTER</Text>
+        <View className="relative w-full top-5 inset-0 mx-auto my-auto space-y-10">
+          <TextInput
+            className="w-[70%] mx-auto h-12 bg-white rounded-xl text-2xl p-2"
+            placeholder="Team Name"
+            onChange={(e) => {
+              setTeamName(e.nativeEvent.text);
+            }}
+          />
+          <TextInput
+            textContentType="password"
+            secureTextEntry={true}
+            className="w-[70%] mx-auto h-12 bg-white rounded-xl text-2xl p-2"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.nativeEvent.text);
+            }}
+          />
+          <TextInput
+            className="w-[70%] mx-auto h-12 bg-white rounded-xl text-2xl p-2"
+            placeholder="Round No"
+            onChange={(e) => {
+              setRoundNo(e.nativeEvent.text);
+            }}
+          />
         </View>
       </View>
-      <Button label={"SUBMIT"} TextColor={ isPress ? "#43FFFF" : "black" }  Margin={40} Press={()=>{}} />
+      <Button
+        label={"SUBMIT"}
+        TextColor={"black"}
+        Margin={40}
+        Press={() => {}}
+      />
     </Background>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    width:400,
-    marginVertical:100,
-    display:"flex",
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center",
+    width: 400,
+    marginVertical: 100,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   heading: {
-    color:"#43FFFF",
-    fontSize:64,
+    color: "#43FFFF",
+    fontSize: 64,
     // top:108,
     // left:60,
-    textAlign:"center",
-    marginBottom:150,
+    textAlign: "center",
+    marginBottom: 150,
     //fontFace:"Aldrich",
-  }, 
-
+  },
 });
 
 export default Register;
